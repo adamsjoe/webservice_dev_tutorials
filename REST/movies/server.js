37f4afde24bd95d67722b86e3902d45ec0a74fe7
Joseph Adams <joseph.adams@blueyonder.co.uk>
2022-09-12 20:01:45 +0100

    used model in get for all movies

--- REST/movies/server.js
+++ REST/movies/server.js
@@ -6,34 +6,33 @@ const port = 3000
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
-mongoose.connect('mongodb+srv://uhi_test:ParkerDenver@cluster0.fpefgo6.mongodb.net/test')
+mongoose.connect('mongodb+srv://testuser:ParkerDenver@webservices.yhgufpp.mongodb.net/Movies')
 const connection = mongoose.connection
-
 connection.on('error', () => {console.error.bind(console, 'error') })
 connection.once('open', () => {
     console.log('Mongoose connected')
 })
 
-app.get('/', (req, res) => {
-    console.log('Recieved GET Request')
-    res.json('GET Data return')
-})
+const MoviesModel = require('./models/moviesModel')
 
 app.get('/movie/:title', (req, res) => {
-    // const movie = movies.find(singleMovie => singleMovie.title == req.params.title )
-    // console.log('Recieved GET Request')
+    // const movie = movies.find(singleMovie => singleMovie.title == req.params.title )    
     res.json(movie)
 })
 
 app.get('/movies/dir/:name', (req, res) => {
     // const dirMovies = movies.filter(dirMovie => dirMovie.dir == req.params.name )
-    // console.log('Recieved GET Request')
     res.json(dirMovies)
 })
 
-app.get('/movies/', (req, res) => {
-    // console.log('Recieved GET Request')
-    // res.json(movies)
+app.get('/movies/', async (req, res) => {
+    // console.log('req >', req, '<')
+    try {
+        const movies = await MoviesModel.find()
+        res.json(movies)
+    } catch (err) {
+        res.status(500).json({message: err.message})
+    }
 })
 
 app.post('/movie/', (req, res) => {
