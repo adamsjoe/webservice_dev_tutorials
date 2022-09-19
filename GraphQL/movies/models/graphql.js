02ac0a1ad68a6ca3fa1e8b387adcf339135a5704
Joseph Adams <joseph.adams@blueyonder.co.uk>
2022-09-19 12:51:06 +0100

    Waiting for info on how to filter

--- GraphQL/movies/models/graphql.js
+++ GraphQL/movies/models/graphql.js
@@ -1,5 +1,7 @@
 const { GraphQLSchema, GraphQLObjectType, GraphQLList, GraphQLNonNull, GraphQLString } = require('graphql')
 
+const dbModal = require('./mongo')
+
 const MovieType = new GraphQLObjectType({
     name: 'Movie',
     description: 'A single movie in the Database',
@@ -12,3 +14,19 @@ const MovieType = new GraphQLObjectType({
 })
 
 exports.movieType = MovieType
+
+const PlatformType = new GraphQLObjectType({
+    name: 'Platform',
+    description: 'A platform with min 1 movie',
+    fields: () => ({
+        platform: { type: GraphQLString },
+        movies: {
+            type: GraphQLList(MovieType),
+            resolve: (movie) => {
+                return dbModal.find({platform: movie.platform})
+            }
+        }
+    })
+})
+
+exports.platformType = PlatformType
--- GraphQL/movies/server.js
+++ GraphQL/movies/server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const { graphqlHTTP } = require('express-graphql')
-const { GraphQLSchema, GraphQLObjectType, GraphQLList } = require('graphql')
+const { GraphQLSchema, GraphQLObjectType, GraphQLList, GraphQLString } = require('graphql')
 const mongoose = require('mongoose')
 // const nodefetch = (...args) => import('node-fetch').then(({default:fetch}) => fetch(...args))
 
@@ -12,6 +12,7 @@ app.use(express.urlencoded({extended: false}))
 
 const dbModal = require('./models/mongo')
 const graphMovies = require('./models/graphql')
+const { aggregate } = require('./models/mongo')
 
 mongoose.connect('mongodb+srv://testuser:ParkerDenver@webservices.yhgufpp.mongodb.net/Movies')
 const connection = mongoose.connection
@@ -20,20 +21,46 @@ connection.once('open', () => {
     console.log('Mongoose connected')
 })
 
-const RootQueryType = new GraphQLObjectType({
+const AllMoviesQuery = new GraphQLObjectType({
     name: 'Query',
     description: 'Root Query',
     fields: () => ({
         movies: {
             type: new GraphQLList(graphMovies.movieType),
             description: 'Returns all movies from the database',
-            resolve: (parent, args) => dbModal.find()
-        }
+            args: {
+                directorName: { type: GraphQLString }
+            },   
+            resolve: (parent, args) => dbModal.find()            
+        },                    
+        platforms: {
+            type: new GraphQLList(graphMovies.platformType),
+            description: 'Returns all platforms in system',
+            resolve: async (parent, args) => {
+                const platforms = await dbModal.find().distinct('platform')
+                let result = []
+                platforms.forEach(platform => result.push({platform: platform}))
+                console.log(result)
+                return result
+            }
+        },
+        platform: {
+            type: graphMovies.platformType,
+            description: 'single platform',
+            args: {
+                platformName: { type: GraphQLString }
+            },
+            resolve: async (parent, args) => {
+                const platform = await dbModal.find().distinct('platform', { platform: args.platformName })
+                return { platform: platform[0] }
+            }
+        },
+
     })
 })
 
 const schema = new GraphQLSchema({
-    query:RootQueryType
+    query:AllMoviesQuery
 })
 
 app.use('/graphql', graphqlHTTP({
